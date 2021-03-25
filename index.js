8afad7b3c77c7083cf9715972febbded9c26c0c9
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-25 17:58:49 +0600

    data collect from form

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require('express');
-
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
 const password = 'osEmLtNQlJWWqY4p';
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbUser007:osEmLtNQlJWWqY4p@Sj@cluster0.b8lgl.mongodb.net/organicdb?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbUser007:osEmLtNQlJWWqY4p@cluster0.b8lgl.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.get("/",(req, res)=>{
+  res.sendFile(__dirname + '/index.html');
+})
+
+
 client.connect(err => {
   const collection = client.db("organicdb").collection("products");
-  console.log('Allahuakbar')
-//   client.close();
+  // perform actions on the collection object
+  app.post("/addProducts",(req, res)=>{
+    const product = req.body;
+    collection.insertOne(product)
+    .then(result=>{
+      res.send('Successfully done !')
+    })
+  })
+  
 });
 
-
-app.get('/',(req, res)=>{
-    res.send('Yaah, it is working..')
-})
 
 
 app.listen(3000);
